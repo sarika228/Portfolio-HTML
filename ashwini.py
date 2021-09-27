bdf1be8e9f24ebfb8cb54e55693a918f63915fee
sarika228 <81947787+sarika228@users.noreply.github.com>
2021-09-27 14:20:32 +0530

    Add files via upload

---
+++ ashwini.py
@@ -0,0 +1,12 @@
+r=0
+while r<=4:
+    c=0
+    while c<=4:
+        if((c==2)and (r!=1 and r!=2 and r!=3)) or ((c==1 or c==3) and(r!=0 and r!=2 and r!=4)) or ((c==0 or c==4) and (r!=1 and r!=0 and r!=3 and r!=4)):
+        
+            print("*",end="")
+        else:
+            print(end=" ")
+        c=c+1
+    print()
+    r=r+1
